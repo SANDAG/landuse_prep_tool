efb96a5be4532abbe4589049101698ac71d495fa
JoeJimFlood <joejimflood@gmail.com>
2024-07-26 13:31:42 -0700

    Updated master script to activate environment in each os.system() call

--- run_landuse_preprocessing.py
+++ run_landuse_preprocessing.py
@@ -2,17 +2,38 @@ import os
 import sys
 import yaml
 
-config_file = sys.argv[1]
-with open(config_file, "r") as f:
-    settings = yaml.safe_load(f)
-    f.close()
+if __name__ == "__main__":
 
-if settings["run_parking_inventory_preprocess"]:
-    os.system(r"1_1_Parking\1_inventory_preprocess\1_parking_preprocess.py " + config_file)
-    print("preprocessed inventory files are written")
-if settings["run_parking_spaces_estimation"]:
-    os.system(r"1_1_Parking\2_spaces_estimation\2_parking_spaces.py " + config_file)
-    print("Initial files are written")
-if settings["run_ABM_preprocess"]:
-    os.system(r"2_ABM_Preprocess\run_preprocess.py " + config_file)
-    print("basic files are written")
+    config_file = sys.argv[1]
+    with open(config_file, "r") as f:
+        settings = yaml.safe_load(f)
+        f.close()
+
+    env_name = os.environ["CONDA_DEFAULT_ENV"]
+
+    if settings["run_parking_inventory_preprocess"]:
+        os.system(
+            r"conda activate {0} && python 1_1_Parking\1_inventory_preprocess\1_parking_preprocess.py {1}".format(
+                env_name,
+                config_file
+            )
+        )
+        print("preprocessed inventory files are written")
+
+    if settings["run_parking_spaces_estimation"]:
+        os.system(
+            r"conda activate {0} && python 1_1_Parking\2_spaces_estimation\2_parking_spaces.py {1}".format(
+                env_name,
+                config_file
+            )
+        )
+        print("Initial files are written")
+        
+    if settings["run_ABM_preprocess"]:
+        os.system(
+            r"conda activate {0} && python 2_ABM_Preprocess\run_preprocess.py {1}".format(
+                env_name,
+                config_file
+            )
+        )
+        print("basic files are written")
